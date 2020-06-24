adde7df41b798e30767bc6e6d560511620c5cbd1
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-06-24 14:08:10 +0700

    Adding activation function

--- src/main.jl
+++ src/main.jl
@@ -9,9 +9,11 @@ end
 function create_layer(from::Int, to::Int)::Layer
     random_weights = rand(from, to)
     random_bias = rand(to)
-    return Layer(random_weights, sum(random_weights, dims=1)[1:end], random_bias, to, from)
+    return Layer(random_weights, (sum(random_weights, dims=1)[1:end] .+ random_bias), random_bias, to, from)
 end
 
 function goto_layer(input::Array{Float64,1}, layer::Layer)::Array{Float64,1}
-    return sum(input .* layer.neurons_weights, dims=1)[1:end] .+ layer.neurons_bias
+    result = (sum(input .* layer.neurons_weights, dims=1)[1:end] .+ layer.neurons_bias) ./ layer.neurons_weights_total
+    result[result .< 0] .= 0
+    return result
 end
