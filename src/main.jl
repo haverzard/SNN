68bf1e9b8bfc158cbadbcb6ffa8d91d0ac37c710
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-08-17 23:21:11 +0700

    Vectorize backpropagation

--- src/main.jl
+++ src/main.jl
@@ -10,19 +10,19 @@ mutable struct Layer
 end
 
 function create_layer(from::Int, to::Int)::Layer
-    random_weights = rand(from, to)
-    random_bias = rand(1,to)
+    random_weights = rand(to, from)
+    random_bias = rand(to, 1)
     return Layer(random_weights, random_bias, to, from)
 end
 
-# We use the classic activation function for now
+# We use the sigmoid activation function for now
 function activation(result::Array{Float64, 2})::Array{Float64, 2}
     result = 1 ./ (1 .+ exp.(-result))
     return result
 end
 
 function goto_layer(input::Array{Float64, 2}, layer::Layer)::Array{Float64, 2}
-    result = (input * layer.neurons_weights) .+ layer.neurons_bias
+    result = (layer.neurons_weights * input) .+ layer.neurons_bias
     return activation(result)
 end
 
@@ -52,11 +52,11 @@ end
 
 function backprop2(neurons::Array{Float64, 2}, layer_weigths::Array{Float64, 2}, errors::Array{Float64, 2})::Array{Float64, 2}
     delta_activation = neurons .* (1 .- neurons)
-    return (errors * transpose(layer_weigths)) .* delta_activation
+    return (transpose(layer_weigths) * errors) .* delta_activation
 end
 
 function backprop4(neurons::Array{Float64,2}, errors::Array{Float64, 2})::Array{Float64, 2}
-    return transpose(neurons) .* errors
+    return errors * transpose(neurons)
 end
 
 function pass_data(input::Array{Float64, 2}, neural::NeuralNetwork)::Array{Float64, 2}
@@ -75,6 +75,7 @@ function train_neuralnetwork(inputs::Array{Float64, 2}, neural::NeuralNetwork, e
     L = neural.total_layers
     
     layer_inputs = inputs
+    push!(layers_outputs, inputs)
     for i in 1:L
         results = goto_layer(layer_inputs, neural.layers[i])
         push!(layers_outputs, results)
@@ -82,31 +83,17 @@ function train_neuralnetwork(inputs::Array{Float64, 2}, neural::NeuralNetwork, e
     end
 
     # Getting the last layer error
-    layers_errors[L] = backprop1(hcat(layers_outputs[L][1,:]...), hcat(expected_results[1,:]...))
-    for i in 2:n
-        layers_errors[L] = layers_errors[L] .+ backprop1(hcat(layers_outputs[L][i,:]...), hcat(expected_results[i,:]...))
-    end
-    layers_errors[L] = layers_errors[L] ./ n
+    layers_errors[L] = backprop1(layers_outputs[L+1], expected_results) ./ n
 
     # Getting the other layer error
     for i in reverse(1:L-1)
-        layers_errors[i] = backprop2(hcat(layers_outputs[i][1,:]...), neural.layers[i+1].neurons_weights, layers_errors[i+1])
-        for j in 2:n
-            layers_errors[i] = layers_errors[i] .+ backprop2(hcat(layers_outputs[i][j,:]...), neural.layers[i+1].neurons_weights, layers_errors[i+1])
-        end
-        layers_errors[i] = layers_errors[i] ./ n
+        layers_errors[i] = backprop2(layers_outputs[i+1], neural.layers[i+1].neurons_weights, layers_errors[i+1]) ./ n
     end
 
     # Calculate gradient descent and learn
-    neural.layers[1].neurons_bias = neural.layers[1].neurons_bias .- (layers_errors[1] .* neural.learning_rate)
-    for j in 1:n
-        neural.layers[1].neurons_weights = neural.layers[1].neurons_weights .- (backprop4(hcat(inputs[j,:]...), layers_errors[1]) .* neural.learning_rate)
-    end
-    for i in 2:L
-        neural.layers[i].neurons_bias = neural.layers[i].neurons_bias .- (layers_errors[i] .* neural.learning_rate)
-        for j in 1:n
-            neural.layers[i].neurons_weights = neural.layers[i].neurons_weights .- (backprop4(hcat(layers_outputs[i-1][j,:]...), layers_errors[i]) .* neural.learning_rate)
-        end
+    for i in 1:L
+        neural.layers[i].neurons_bias = neural.layers[i].neurons_bias .- (sum(layers_errors[i], dims=2) .* neural.learning_rate)
+        neural.layers[i].neurons_weights = neural.layers[i].neurons_weights .- (backprop4(layers_outputs[i], layers_errors[i]) .* neural.learning_rate)
     end
 end
 
